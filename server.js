d46af08db2fe50dd5598b95f2bc6c9e4b6807b8f
dvpeixoto <di.villanova@gmail.com>
2016-08-12 09:34:38 -0300

    Adjusted comments in server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var server = require('http').createServer(app);
 
 var io = require('socket.io').listen(server);
 
+//Verifica se a aplicação está sendo rodada do openshift ou local
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
